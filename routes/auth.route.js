35b27be8e975a211c38ccc1ca24dbffe3c6aef93
ducnghiem2411 <ducnghiem2411@gmail.com>
2020-08-14 16:20:17 +0700

    modify register route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const { login, register } = require('../controllers/auth.controller')
 
-router.post('/register', register)
+router.post('/', register)
 router.post('/login', login)
 
 module.exports = router
