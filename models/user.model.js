6155459ecfd774c2825c1feb2ecf8166a7cff4b9
ducnghiem2411 <ducnghiem2411@gmail.com>
2020-08-18 10:00:23 +0700

    add user model validation

--- models/user.model.js
+++ models/user.model.js
@@ -1,17 +1,30 @@
 const mongoose = require('mongoose')
 
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
+    minlength: 6,
     required: true
   },
   password: {
     type: String,
+    minlength: 6,
     required: true
   },
-  email: String,
+  email: {
+    type: String,
+    validate: [validateEmail, 'Invalid email!']
+  },
   image : String,
-  bio : String
+  bio : {
+    type: String,
+    maxlength: 150,
+  }
 })
 
 const User = mongoose.model('User', userSchema)
