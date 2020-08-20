1e6d7455ede116fe03408237e0114c4af598ea4b
ducnghiem2411 <ducnghiem2411@gmail.com>
2020-08-20 17:50:45 +0700

    modify responses

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,23 +2,23 @@ const { registerService, loginService } = require('../services/auth.services')
 const { generateJWT, verifyToken } = require('../services/token.services')
 
 const register = async (req, res) => {
-  const { username, password, email } = req.body.user
+  const { username, email, password } = req.body.user
   try {
-    let result = await registerService(username, password, email)
-    res.status(200).send({user: result})
+    let result = await registerService(username, email, password)
+    res.status(201).send({user: result})
   } catch (error) {
-    res.status(422).send({error : error})
+    res.status(422).send(error)
   }
 }
 
 const login = async (req, res) => {
-  const { username, password } = req.body
+  const { username, password } = req.body.user
   try {
     let result = await loginService(username, password) //return username
     let token = generateJWT(result)
-    res.status(200).send({username: username, token : token})
+    res.status(200).send({user: result})
   } catch (error) {
-    res.status(422).send({error : error})
+    res.status(422).send(error)
   }
 }
 
