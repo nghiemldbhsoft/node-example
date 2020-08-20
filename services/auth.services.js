7ace5fe6ae7625fc6fa052f6a5de6c9db900fd83
ducnghiem2411 <ducnghiem2411@gmail.com>
2020-08-20 17:55:32 +0700

    update

--- services/auth.services.js
+++ services/auth.services.js
@@ -1,26 +1,45 @@
 const { User } = require('../models/user.model')
 const statusMessages = require('../utils/types')
+const { generateJWT } = require('../services/token.services')
 
-const registerService = async (username, password, email) => {
+const registerService = async (username, email, password) => {
   const user = await User.findOne({ username: username })
+  console.log('user in db yet?', user)
   if (user) {
     throw new Error(statusMessages._422)
   } else {
+    let token = generateJWT(username)
+    console.log(token)
     const newUser = new User({
+      email: email,
+      token: token,
       username: username,
       password: password,
-      email: email
+      bio: '',
+      image: ''
     })
-    return newUser.save()
+    let response = await newUser.save()
+    console.log('saved user', response)
+    let result = {
+      email: response.email,
+      token: response.token,
+      username: response.username,
+      bio: response.bio,
+      image: response.image
+    }
+    return result
   }
 }
 
 const loginService = async (username, password) => {
-  const user = await User.findOne({ username: username, password: password })
+  const user = await User.findOne({
+    username: username,
+    password: password
+  }).select(['-password', '-_id', '-__v'])
   if (user) {
-    return user.username
+    return user
   } else {
-    throw new Error(statusMessages._422)
+    throw new Error(statusMessages._401)
   }
 }
 
